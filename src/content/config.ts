ddb245e08b06c68a46f0c08698f13663c8da00d5
Keigo Kusumegi <kusukun7@gmail.com>
2023-08-20 06:20:58 -0400

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,17 @@
-// 1. `astro:content` からユーティリティをインポート。
-// ここで **`astro:content` が見つからん** って怒られる人は本記事の一番下参照
-import { defineCollection } from 'astro:content';
-import { blogSchema, newsletterSchema } from '../schemas';
-// import { blogSchema } from 'my-blog-theme'; // サードパーティ製でも 🙆🏻‍♂️
+// // 1. `astro:content` からユーティリティをインポート。
+// // ここで **`astro:content` が見つからん** って怒られる人は本記事の一番下参照
+// import { defineCollection } from 'astro:content';
+// import { blogSchema, newsletterSchema } from '../schemas';
+// // import { blogSchema } from 'my-blog-theme'; // サードパーティ製でも 🙆🏻‍♂️
 
-// 2. 検証したいコレクションごとにスキーマを定義する。
-const blogCollection = defineCollection({ schema: blogSchema });
-const newsletterCollection = defineCollection({ schema: newsletterSchema });
+// // 2. 検証したいコレクションごとにスキーマを定義する。
+// const blogCollection = defineCollection({ schema: blogSchema });
+// const newsletterCollection = defineCollection({ schema: newsletterSchema });
 
-// 3. コレクションを登録するために `collections` オブジェクトをエクスポート。
-// `key` が 1. で作った `<コレクション名>` に対応している。
-export const collections = {
-  // blog: defineCollection({ /* ... */ }), // 直接書いても 🙆🏻‍♂️
-  blog: blogCollection,
-  newsletter: newsletterCollection,
-};blogPost
+// // 3. コレクションを登録するために `collections` オブジェクトをエクスポート。
+// // `key` が 1. で作った `<コレクション名>` に対応している。
+// export const collections = {
+//   // blog: defineCollection({ /* ... */ }), // 直接書いても 🙆🏻‍♂️
+//   blog: blogCollection,
+//   newsletter: newsletterCollection,
+// };
